0b408930ab0c04746a1854b526187c61d24e70f8
ruman-dev <ruman.uits@gmail.com>
2023-07-09 23:18:25 +0600

    latest release

--- lib/main.dart
+++ lib/main.dart
@@ -3,42 +3,43 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 60,
-                  backgroundImage: AssetImage("images/ruman.jpg")
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage("images/ruman.jpg"),
+              ),
+              Text(
+                "Md. Ruman",
+                style: TextStyle(
+                  fontFamily: 'Belanosima',
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                Text(
-                  "Md. Ruman",
-                      style: TextStyle(
-                        fontFamily: 'Belanosima',
-                        fontSize: 30,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+              ),
+              Text(
+                "Android Developer",
+                style: TextStyle(
+                  fontFamily: 'Belanosima',
+                  fontSize: 18,
+                  letterSpacing: 2,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                ),
-                Text(
-                  "Android Developer",
-                  style: TextStyle(
-                    fontFamily: 'Belanosima',
-                    fontSize: 18,
-                    letterSpacing: 2,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+              ),
               SizedBox(
                 height: 40,
                 width: 150,
@@ -46,40 +47,40 @@ class MyApp extends StatelessWidget {
                   color: Colors.white,
                 ),
               ),
-                Card(
-                  margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      "+8801994385596",
-                        style: TextStyle(
-                          fontFamily: 'Belanosima-Regular.ttf',
-                          fontSize: 17,
-                        ),
-                    ),
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email_outlined,
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
                     color: Colors.teal,
-                    ),
-                    title: Text(
-                      "ruman.dev@outlook.com",
-                      style: TextStyle(
-                        fontFamily: 'Belanosima-Regular.ttf',
-                        fontSize: 17,
-                      ),
+                  ),
+                  title: Text(
+                    "+8801994385596",
+                    style: TextStyle(
+                      fontFamily: 'Belanosima-Regular.ttf',
+                      fontSize: 17,
                     ),
                   ),
                 ),
-             ],
-            ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email_outlined,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "ruman.dev@outlook.com",
+                    style: TextStyle(
+                      fontFamily: 'Belanosima-Regular.ttf',
+                      fontSize: 17,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
